bee819fb3c24a08a0d1669cead8c8a659bb1bdda
mirek <mirek114@gmail.com>
2015-04-18 10:59:04 +0200

    Nieprzechodzacy test pierwszej metody Dodano dwa projekty: wlasciwa aplikacje konsolowa oraz proejekt zawierajacy testy do niej. Uworzono nieprzechodzacy test dla pierszej metody.

---
+++ PrimeCalculator.Test/PrimeNumberTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PrimeCalculator;
+
+namespace PrimeCalculator.Test
+{
+    [TestClass]
+    public class PrimeNumberTest
+    {
+        [TestMethod]
+        public void IsPrimeTest()
+        {
+            PrimeNumber primeNumber = new PrimeNumber();
+
+            Assert.IsTrue(primeNumber.IsPrime(3));
+        }
+    }
+}
---
+++ PrimeCalculator/PrimeNumber.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeCalculator
+{
+    public class PrimeNumber
+    {
+        public bool IsPrime(int number)
+        {
+            return false;
+        }
+    }
+}
